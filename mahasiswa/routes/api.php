e34f228b52bdeeffee075c6a5f6fc4fdf3e5773e
sevyra <sevyra02@gmail.com>
2022-12-23 14:07:49 +0700

    update api

--- mahasiswa/routes/api.php
+++ mahasiswa/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('mahasiswa', [MahasiswaController::class, 'index']);
+Route::post('mahasiswa/store', [MahasiswaController::class, 'store']);
+Route::get('mahasiswa/show/{id}', [MahasiswaController::class, 'show']);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
